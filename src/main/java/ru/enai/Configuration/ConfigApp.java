e24e5f36e3b93b629663185fe5ef560f3f9e34b0
AntonPetrov <Toha76@yandex.ru>
2020-11-17 14:15:17 +0400

    Initial commit

---
+++ src/main/java/ru/enai/Configuration/ConfigApp.java
@@ -0,0 +1,9 @@
+package ru.enai.Configuration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+public class ConfigApp {
+}
---
+++ src/main/java/ru/enai/bot/WeatherBot.java
@@ -0,0 +1,50 @@
+package ru.enai.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class WeatherBot extends TelegramLongPollingBot {
+    @Value(value = "${spring.bot.name")
+    private String botName;
+    @Value(value = "${spring.bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        //check if the update has a message
+        if(update.hasMessage()){
+            Message message = update.getMessage();
+
+            //check if the message has text. it could also  contain for example a location ( message.hasLocation() )
+            if(message.hasText()){
+
+                //create a object that contains the information to send back the message
+                SendMessage sendMessageRequest = new SendMessage();
+                sendMessageRequest.setChatId(message.getChatId().toString()); //who should get the message? the sender from which we got the message...
+                sendMessageRequest.setText("you said: " + message.getText());
+                try {
+                    execute(sendMessageRequest); //at the end, so some magic and send the message ;)
+                } catch (TelegramApiException e) {
+                    //do some error handling
+                }//end catch()
+            }//end if()
+        }//end  if()
+
+    }
+}
